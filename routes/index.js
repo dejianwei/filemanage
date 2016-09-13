960ee0ce4b2654dd2e6b9ca4190621df0fd02979
dejianwei <295808954@qq.com>
2016-09-13 10:29:44 +0800

    init project

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.render('index');
+    });
+
+    app.get('/yuguidang', function(req, res) {
+
+    });
+
+    app.get('/guidang', function(req, res) {
+
+    });
+}
